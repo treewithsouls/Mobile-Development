1d1ff630368fe0bb2f1d49b95cdc857c18f0e503
treewithsouls <leaflesstree@vk.com>
2024-12-21 15:22:03 +0300

    Update main.dart

--- lib3/main.dart
+++ lib3/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:testappgulyaev/presentation/home_page/home_page.dart';
+import 'package:testappkirsanov/presentation/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Гуляев Д.А. ВТАСбзу-31'),
+      home: const MyHomePage(title: 'Кирсанов В.А. ВТАСбзу-31'),
     );
   }
 }
